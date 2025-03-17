7f33105f66947b3099fe8e8316fe0b54860c0393
LazarStankovic03 <lazar.stankovic@fazi.rs>
2025-03-17 22:14:28 +0100

    ne radi

--- src/app/features/filmovi/filmovi.component.ts
+++ src/app/features/filmovi/filmovi.component.ts
@@ -1,22 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilmoviService } from './filmovi.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filmovi',
   standalone: true,
-  imports: [CommonModule], // DODAJ OVO!
+  imports: [CommonModule, FormsModule],
   templateUrl: './filmovi.component.html',
   styleUrls: ['./filmovi.component.css']
 })
 export class FilmoviComponent {
   filmovi: any[] = [];
+  filteredFilmovi: any[] = [];
+  searchTitle: string = '';
+  searchDirector: string = '';
+  searchYear: string = '';
 
   constructor(private filmoviService: FilmoviService) { }
 
   ngOnInit(): void {
     this.filmoviService.getFilmovi().subscribe((data: any[]) => {
       this.filmovi = data;
+      this.filteredFilmovi = data; // Postavljamo inicijalnu listu
     });
+  }
+
+  // Filtriranje filmova prema naslovu, režiseru i godini
+  filterMovies(): void {
+    const selectedYear = this.searchYear ? new Date(this.searchYear).getFullYear().toString() : '';
+
+    this.filteredFilmovi = this.filmovi.filter(film =>
+      film.title.toLowerCase().includes(this.searchTitle.toLowerCase()) &&
+      film.director.name.toLowerCase().includes(this.searchDirector.toLowerCase()) &&
+      (selectedYear ? film.startDate.toString().includes(selectedYear) : true)
+    );
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,5 @@ bootstrapApplication(AppComponent, {
     provideHttpClient()
   ]
 }).catch((err) => console.error(err));
+
+
