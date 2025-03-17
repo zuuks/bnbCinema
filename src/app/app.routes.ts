9d7686e4bc9b9a570796144ed38f9ede0027ff15
Baxurius <64602299+Baxurius@users.noreply.github.com>
2025-03-17 19:56:22 +0100

    kurac
    KURCINELA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './features/home/home.component';
-
+import { LoginComponent } from './features/login/login.component';
+import { SignupComponent } from './features/signup/signup.component';
 
 import { ContactComponent } from './features/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, },
-
+    
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
 
     { path: 'contact', component: ContactComponent },
 
