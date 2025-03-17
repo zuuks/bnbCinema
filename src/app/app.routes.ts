b5808ae4a5fc4fad94ece4aa5b25b3fb9dc49e61
zuuks <53404260+zuuks@users.noreply.github.com>
2025-03-17 20:01:43 +0100

    ss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './features/login/login.component';
 import { SignupComponent } from './features/signup/signup.component';
+import { FilmoviComponent } from './features/filmovi/filmovi.component';
 
 import { ContactComponent } from './features/contact/contact.component';
 
@@ -11,6 +12,7 @@ export const routes: Routes = [
     
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'filmovi', component: FilmoviComponent },
 
     { path: 'contact', component: ContactComponent },
 
