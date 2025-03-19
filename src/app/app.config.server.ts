2fd0b7df4b831a857fba4debb4a950317860b935
LazarStankovic03 <lazar.stankovic@fazi.rs>
2025-03-19 19:26:33 +0100

    d

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,7 +3,16 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
 
+export const authenticationInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next:
+  HttpHandlerFn) => {
+  const modifiedReq = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${sessionStorage.getItem('token')}`),
+  });
+
+  return next(modifiedReq);
+};
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
